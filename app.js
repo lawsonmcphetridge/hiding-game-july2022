678c0bbc629fe21c0af6e241d5de3233df3186dd
Lawson <lawsonmcphetridge2020@gmail.com>
2022-07-26 13:00:40 -0700

    added javascript to get random cup

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
 // import functions and grab DOM elements
+const firstButtonEl = document.getElementById('first-button');
+const secondButtonEl = document.getElementById('second-button');
+const thirdButtonEl = document.getElementById('third-button');
+
+const cupOneEl = document.getElementById('cup-one');
+const cupTwoEl = document.getElementById('cup-two');
+const cupThreeEl = document.getElementById('cup-three');
+
+const winsEl = document.getElementById('wins');
+const lossesEl = document.getElementById('losses');
+const totalEl = document.getElementById('total');
+
+const hidingLogo = ['firstCup', 'secondCup', 'thirdCup']
+
+let totalGuesses = 0;
+let wins = 0;
+let losses = 0;
 
 // let state
+firstButtonEl.addEventListener('click', () => {
+  const hidingCup = Math.floor(Math.random() * hidingLogo.length);
+  const hidingSpot = hidingCup[hidingLogo];
+  handleGuess(hidingSpot, 'firstCup')
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+});
+
+secondButtonEl.addEventListener('click', () => {
+  const hidingCup = Math.floor(Math.random() * hidingLogo.length);
+  const hidingSpot = hidingLogo[hidingCup];
+  handleGuess(hidingSpot, 'secondCup')
+});
+
+thirdButtonEl.addEventListener('click', () => {
+  const hidingCup = Math.floor(Math.random() * hidingLogo.length);
+  const hidingSpot = hidingCup[hidingLogo];
+  handleGuess(hidingSpot, 'thirdCup')
+});
+
+
+function handleGuess(userGuess, correctSpot) {
+
+  if (correctSpot === 'first cup') {
+  cupOneEl.src=
+}
+
+
+
+}
+
+
+
+
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
